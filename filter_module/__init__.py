c28d2ea06f6c13be9415e08c264989210d479e44
Fertuesta25 <59838508+Fertuesta25@users.noreply.github.com>
2025-05-04 18:28:31 -0500

    Create __init__.py

---
+++ filter_module/__init__.py
@@ -0,0 +1,9 @@
+"""
+Plugin de Filtrado para QGIS
+Este plugin crea un panel lateral donde se puede seleccionar un campo
+y filtrar la capa activa por los valores de ese campo.
+"""
+
+# Esta función es necesaria para que QGIS cargue el plugin
+def classFactory(iface):
+    return FilterPlugin(iface)
