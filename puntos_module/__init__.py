cbeab5bbc56aabf428bdbc2b8384173ab87a161c
Fertuesta25 <59838508+Fertuesta25@users.noreply.github.com>
2025-05-04 18:39:01 -0500

    Create __init__.py

---
+++ puntos_module/__init__.py
@@ -0,0 +1,7 @@
+"""
+Módulo para generar puntos a lo largo de líneas en QGIS
+Parte del plugin AGROFT Precisión
+Implementado como un panel lateral
+"""
+
+from .puntos_linea import get_module_instance
